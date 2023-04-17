08fe96b04b42b35ed1f24d9e03c7085dcbfc7a5c
Matas Armonaitis <matas.armonaitis@gmail.com>
2023-04-17 13:02:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createHashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Pokedex from './Pokedex';
 import PokemonDetails from './PokemonDetails';
 
